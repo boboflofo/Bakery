d5817ff5c75fa05e8b27764c75aedea4ef7936f3
Jonathan Song <jonats10@uci.edu>
2024-01-19 13:36:16 -0800

    add pastry auto-implemented test, red

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -12,5 +12,16 @@ namespace Bakery.Tests
       Pastry newPastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+
+    [TestMethod]
+
+    public void GetPrice_ReturnsPrice_Int()
+    {
+      int price = 5;
+      Pastry newPastry = new Pastry(1);
+
+      int result = newPastry.Price;
+      Assert.AreEqual(price,result);
+    }
   }
 }
